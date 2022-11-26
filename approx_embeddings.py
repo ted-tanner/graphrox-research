05506e7d5541b14e19727e9fa88e8fced3fe4b9c
Tanner Davies <tanner.e.davies@gmail.com>
2022-11-25 22:16:22 -0700

    Better utilize the CPU

--- approx_embeddings.py
+++ approx_embeddings.py
@@ -7,48 +7,10 @@ import graphrox as gx
 import networkx as nx
 import pickle as pkl
 
-from multiprocessing import Manager, Process, Queue
+from multiprocessing import Manager, Pool, Queue, Value
 
 from karateclub import FeatherGraph, LDP, Graph2Vec
 from karateclub.dataset import GraphSetReader
-
-
-def generate_graphset_portion(work_queue, nx_graphset, n, m):
-    while not work_queue.empty():
-        count = work_queue.get()
-        
-        for i in range(count):
-            graph = nx.barabasi_albert_graph(n, m, seed=int(time.time() + i))
-
-            nx_graphset.append(graph)
-        
-
-def generate_barabasi_albert_graphset(n, m, count):
-    """Generates a set of `count` Barabasi-Albert graphs with the given `n` and `m`"""
-    work_queue = Queue()
-    manager = Manager()
-    nx_graphset = manager.list()
-
-    extra = count - (int(count / os.cpu_count()) * os.cpu_count())
-
-    for i in range(os.cpu_count()):
-        workload = count if i != 0 else count + extra
-        work_queue.put(workload)
-
-    processes = []
-
-    for i in range(os.cpu_count()):
-        process = Process(target=generate_graphset_portion, args=(work_queue,
-                                                                  nx_graphset,
-                                                                  n,
-                                                                  m))
-        process.start()
-        processes.append(process)
-
-    for process in processes:
-        process.join()
-
-    return nx_graphset
 
 
 def get_embeddings(graphs, approx_graphs, model):
@@ -71,20 +33,16 @@ def get_embeddings(graphs, approx_graphs, model):
     }
 
     return (embeddings, approx_embeddings)
+        
 
+def generate_barabasi_albert_graphset(n, m, count):
+    """Generates a set of `count` Barabasi-Albert graphs with the given `n` and `m`"""
+    graphset = []
 
-def process_embeddings(work_queue, embeddings, graphs, approx_graphs):
-    while not work_queue.empty():
-        job = work_queue.get()
+    for i in range(count):
+        graphset.append(nx.barabasi_albert_graph(n, m, seed=int(time.time() + i)))
 
-        emb, approx_emb = get_embeddings(graphs, approx_graphs, job['model'])
-        embeddings.append({
-            'name': job['name'],
-            'standard': emb,
-            'approximate': approx_emb,
-        })
-
-        print('Generated ' + job['name'] + ' embeddings')
+    return graphset
 
         
 def networkx_to_graphrox(nx_graph):
@@ -117,10 +75,100 @@ def graphrox_to_networkx(gx_graph):
     return nx_graph
 
 
-def approximate_gx_graphs(gx_graphs, block_dimension, threshold):
-    """Returns duplicated and approximated versions of the gx_graphs"""
-    return [g.duplicate().approximate(block_dimension, threshold) for g in gx_graphs]
+def process_embeddings(params, tasks_completed):
+    print('Processing embeddings for task ' + str(params['id']) + '...')
 
+    graphs = params['graphs']
+    approx_graphs = params['approx_graphs']
+
+    embeddings = []
+
+    emb, approx_emb = get_embeddings(params['graphs'], params['approx_graphs'], FeatherGraph())
+    embeddings.append({
+        'name': 'FeatherGraph',
+        'standard': emb,
+        'approximate': approx_emb,
+    })
+
+    emb, approx_emb = get_embeddings(params['graphs'], params['approx_graphs'], LDP())
+    embeddings.append({
+        'name': 'LDP',
+        'standard': emb,
+        'approximate': approx_emb,
+    })
+
+    emb, approx_emb = get_embeddings(params['graphs'], params['approx_graphs'], Graph2Vec())
+    embeddings.append({
+        'name': 'Graph2Vec',
+        'standard': emb,
+        'approximate': approx_emb,
+    })
+
+    count = params['count']
+    n = params['n']
+    m = params['m']
+    block_dimension = params['block_dimension']
+    threshold = params['threshold']
+
+    data = {
+        'gen_n': n,
+        'gen_m': m,
+        'approx_block_dimension': block_dimension,
+        'approx_threshold': threshold,
+        'graph_count': count,
+        'embeddings': embeddings,
+    }
+
+    filename = f'out/emb_c({count})_n({n})_m({m})_b({block_dimension})_t({threshold}).pkl'
+    with open(filename, 'wb') as out_file:
+        pkl.dump(data, out_file)
+
+    with tasks_completed.get_lock():
+        tasks_completed.value += 1
+        completed = tasks_completed.value
+
+    task_id = params['id']
+    tasks_total = params['tasks_total']
+        
+    print(f'Completed task {task_id}. {completed}/{tasks_total} complete')
+
+
+def prepare_graphs(params, emb_queue):
+    print('Preparing for task ' + str(params['id']) + '...')
+
+    nx_graphs = generate_barabasi_albert_graphset(params['n'], params['m'], params['count'])
+    gx_graphs = []
+
+    for graph in nx_graphs:
+        gx_graphs.append(networkx_to_graphrox(graph))
+
+    gx_approx_graphs = [
+        g.approximate(params['block_dimension'], params['threshold']) for g in gx_graphs
+    ]
+
+    nx_approx_graphs = []
+
+    for graph in gx_approx_graphs:
+        nx_approx_graphs.append(graphrox_to_networkx(graph))
+
+    print('Finished preparation for task ' + str(params['id']) + '.')
+
+    params['graphs'] = nx_graphs
+    params['approx_graphs'] = nx_approx_graphs
+    emb_queue.put(params)
+
+
+def dispatch_worker(prep_queue, emb_queue, tasks_completed):
+    while True:
+        if not emb_queue.empty():
+            task = emb_queue.get()
+            process_embeddings(task, tasks_completed)
+        elif not prep_queue.empty():
+            task = prep_queue.get()
+            prepare_graphs(task, emb_queue)
+        else:
+            time.sleep(0.25)
+    
 
 if __name__ == '__main__':
     try:
@@ -143,79 +191,54 @@ if __name__ == '__main__':
         print('Error: Tasks list not found in emb_config.json ')
         sys.exit(1)
 
+    print('There are a total of ' + str(len(tasks)) + ' tasks.')
+    print()
+
+    if not os.path.exists('out'):
+        os.makedirs('out')
+
+    prep_queue = Queue()
+    emb_queue = Queue()
+
+    tasks_completed = Value('i', 0)
+
     for i, task in enumerate(conf['tasks']):
         task_id = i + 1
-        try: 
-            n = task['n']
-            m = task['m']
-            block_dimension = task['block_dimension']
-            threshold = task['threshold']
-            count = task['count']
-        except KeyError:
+
+        are_task_params_valid = True
+
+        if 'n' not in task:
+            are_task_params_valid = False
+
+        if 'm' not in task:
+            are_task_params_valid = False
+
+        if 'block_dimension' not in task:
+            are_task_params_valid = False
+
+        if 'threshold' not in task:
+            are_task_params_valid = False
+
+        if 'count' not in task:
+            are_task_params_valid = False
+
+        if not are_task_params_valid:
             print('Error: Configuration for task ' + str(task_id) + ' is invalid')
-            sys.exit(1)
+            continue
 
-        if i != 0:
-            print('----------------------------------------------------------------')
-            print()
+        task['id'] = task_id
+        task['tasks_total'] = len(tasks)
 
-        print('Beginning task ' + str(task_id))
-        print()
+        prep_queue.put(task)
 
-        print('Generating graphs...')
+    worker_pool = Pool(os.cpu_count(), dispatch_worker, (prep_queue, emb_queue, tasks_completed,))
 
-        nx_graphs = generate_barabasi_albert_graphset(n, m, count)
-        gx_graphs = []
+    while True:
+        if tasks_completed.value == len(tasks):
+            sys.exit(0)
+        else:
+            time.sleep(1)
 
-        for graph in nx_graphs:
-            gx_graphs.append(networkx_to_graphrox(graph))
+        
 
-        print('Approximating graphs...')
-        gx_approx_graphs = approximate_gx_graphs(gx_graphs, block_dimension, threshold)
-
-        nx_approx_graphs = []
-
-        for graph in gx_approx_graphs:
-            nx_approx_graphs.append(graphrox_to_networkx(graph))
-
-        work_queue = Queue()
-        manager = Manager()
-        embeddings = manager.list()
-
-        work_queue.put({'name': 'FeatherGraph', 'model': FeatherGraph()})
-        work_queue.put({'name': 'LDP', 'model': LDP()})
-        work_queue.put({'name': 'Graph2Vec', 'model': Graph2Vec()})
-
-        print('Generating embeddings...')
-        print()
-
-        processes = []
-
-        for _ in range(os.cpu_count()):
-            if not work_queue.empty():
-                process = Process(target=process_embeddings, args=(work_queue,
-                                                                   embeddings,
-                                                                   nx_graphs,
-                                                                   nx_approx_graphs))
-                process.start()
-                processes.append(process)
-
-        for process in processes:
-            process.join()
-
-        data = {
-            'gen_n': n,
-            'gen_m': m,
-            'approx_block_dimension': block_dimension,
-            'approx_threshold': threshold,
-            'graph_count': count,
-            'embeddings': embeddings,
-        }
-
-        filename = f'emb_c({count})_n({n})_m({m})_b({block_dimension})_t({threshold}).pkl'
-        with open(filename, 'wb') as out_file:
-            pkl.dump(data, out_file)
-
-        print()
-        print('Completed task ' + str(task_id))
-        print()
+        
